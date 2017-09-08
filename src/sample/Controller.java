79a31cbb2ab4a65078ea647812c9468014548cc3
blackbird1840 <birdblack1840@yahoo.com>
2017-09-08 09:51:57 +0200

    added some comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,6 @@
 package sample;
 
-import applicationsForHausaufgabe.AnaylseApplication;
+import applicationsForHausaufgabe.StepStoneTestApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -10,47 +10,58 @@ import tools.CheckInputURL;
 import java.util.Iterator;
 import java.util.Map;
 
-
+/**
+ * conbine the View and the model
+ */
 public class Controller {
 
+    /**
+     * websiteToBeanalyse input in the UI
+     * showReults output in UI
+     * */
     @FXML
     private TextField websiteToBeAnalyse;
     @FXML
     private TextArea showResults;
 
+    /**
+     * "button" controller
+     */
     @FXML
     protected void submitWebsiteToAnalyse(){
-        System.out.println(websiteToBeAnalyse.getText());
-        //
+        /** check, if input is with "protocol" information*/
         if(CheckInputURL.isURLWithHeader(websiteToBeAnalyse.getText()))
         {
             WebsiteInformation websiteInformation = new WebsiteInformation(websiteToBeAnalyse.getText(),"a");
+            /** check, if the website is reachable*/
             if(websiteInformation.getSelectedElements() != null) {
-                AnaylseApplication anaylseApplication = new AnaylseApplication();
-                System.out.println(websiteInformation.getSelectedElements().toString());
+                StepStoneTestApplication anaylseApplication = new StepStoneTestApplication();
                 anaylseApplication.frenquenyOfHrefWithWebAdresse(websiteInformation.getSelectedElements());
 
-                // write out the results, first clear the TextArea
+                /** write out the results, first clear the TextArea*/
                 showResults.clear();
+                /** get information from the Site-Frenquency and build the strings to output*/
                 Iterator iter = anaylseApplication.getWordAndFrenquency().entrySet().iterator();
                 while (iter.hasNext()) {
                     Map.Entry entry = (Map.Entry) iter.next();
-
-                    // write out
+                    /** build string to output*/
                     showResults.appendText(entry.getKey().toString() + " - ");
                     showResults.appendText(entry.getValue().toString() + "\n");
                 }
             }
+            /** case: unreachable website*/
             else {
                 showResults.clear();
                 showResults.appendText("please check the adress, the URL is not reachable ");
             }
-
         }
+        /** case: input without "protocol"*/
         else {
             showResults.clear();
-            showResults.appendText("please check the adress, what you put in. maybe 1.you forget 'http'or 'https' ");
-        }
+            showResults.appendText("please check the adress, what you put in. " +
+                    "\n 1. maybe you forget 'http://'or 'https://:' " +
+                    "\n 2. you forgot the domain");
 
+        }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Stepstone's homework");
         primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
     }

2760683318ab7cfdf415b5c695d9ba5c7706ac50
blackbird1840 <birdblack1840@yahoo.com>
2017-09-08 09:52:20 +0200

    added some comments

--- src/tools/CheckInputURL.java
+++ src/tools/CheckInputURL.java
@@ -3,8 +3,12 @@ package tools;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * here are some small tool functions, which can be reused.
+ */
 public class CheckInputURL {
 
+    /** check, if the input with "transfer protocol". In this homework, "http" and "https" are concerned */
     public static boolean isURLWithHeader(String inputURL){
         String regex = "^(http|https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
         Pattern pattern = Pattern.compile(regex);

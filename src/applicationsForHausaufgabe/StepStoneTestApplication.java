75f0f9e7a11fb3ad92b83cc7d2e089e8fdd71094
blackbird1840 <birdblack1840@yahoo.com>
2017-09-08 09:50:40 +0200

    added some comments

--- src/applicationsForHausaufgabe/StepStoneTestApplication.java
+++ src/applicationsForHausaufgabe/StepStoneTestApplication.java
@@ -7,11 +7,11 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class AnaylseApplication {
+public class StepStoneTestApplication {
 
     private HashMap<String,Integer> wordAndFrenquency;
 
-    public AnaylseApplication() {
+    public StepStoneTestApplication() {
         this.wordAndFrenquency = new HashMap<>();
     }
 
@@ -19,41 +19,46 @@ public class AnaylseApplication {
         return wordAndFrenquency;
     }
 
-    /*
-          in this case get the webadresse and count them
-        */
+    /**
+     * analyse info and save the data in hashmap
+     * @param elementsTobeAnalyse  the date to be analysed
+     * @return hashmap of "Website-frenquencyOfWebsite"
+     */
     public HashMap<String,Integer> frenquenyOfHrefWithWebAdresse(Elements elementsTobeAnalyse){
-        // find the href with "http" or "https"
         // TODO may be there another ends
+        /** we may later add some new domains
+         * use regex to parse source words, take out the parts and build as new words*
+         */
         Pattern pattern = Pattern.compile("(.*)(http://|https://)(.*)(.pl|.com|.de|.com.cn)(/.*)");
-
         for (Element element: elementsTobeAnalyse) {
+            /** get and parse the source element*/
             Matcher matcher = pattern.matcher(element.toString());
             while(matcher.find()) {
+                /** build new words*/
                 StringBuilder webAdress = new StringBuilder();
-
                 webAdress.append(matcher.group(3));
                 webAdress.append(matcher.group(4));
-//                System.out.println(webAdress);
-
-                constructHashmapWebsiteAdresseAndFrenquency(webAdress.toString());
+                /** save then in hashmap*/
+                constructTableWebsiteAdresseAndFrenquency(webAdress.toString());
             }
         }
         return this.wordAndFrenquency;
     }
 
 
-    /*
-    @return construct the hashmap
+    /**
+     *
+     *@param webSiteToInsert the word, need to save
+     * construct the hashmap
      */
-    private void constructHashmapWebsiteAdresseAndFrenquency(String webSiteToInsert){
-        // webAdresse existed
+    private void constructTableWebsiteAdresseAndFrenquency(String webSiteToInsert){
+        /** webAdresse existed, then add 1 to the frenquency*/
         if(this.wordAndFrenquency.containsKey(webSiteToInsert)){
             Integer integer = this.wordAndFrenquency.get(webSiteToInsert);
             integer++;
             this.wordAndFrenquency.put(webSiteToInsert,integer);
         }
-       // webAdresse not existed
+       /** webAdresse not existed,then insert it*/
         else
             this.wordAndFrenquency.put(webSiteToInsert,1);
     }

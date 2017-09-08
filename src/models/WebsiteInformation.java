1937d731fe6e23ee8f7918b732c355e7b8a99d3a
blackbird1840 <birdblack1840@yahoo.com>
2017-09-08 09:51:33 +0200

    added some comments

--- src/models/WebsiteInformation.java
+++ src/models/WebsiteInformation.java
@@ -6,10 +6,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class WebsiteInformation {
+
     private String webSiteToBeAnalyse;
+    /**the Tag, whose elements we want to.*/
     private String selectTagInWebsite;
 
     public WebsiteInformation(String webSiteToBeAnalyse, String selectTagInWebsite) {
@@ -18,10 +21,11 @@ public class WebsiteInformation {
         this.selectTagInWebsite = selectTagInWebsite;
     }
 
-
-    /*
-          @return the data of the website or null
-        */
+    /**
+     * 1. get all information from the Website
+     * 2. handel with some Exceptions, which can hanppen. If so, we will get "null"
+     * @return the data of the website or null
+     */
     private Document getAllDataFromWebSite() {
         try {
             Document dataFromWebsite = Jsoup.connect(this.webSiteToBeAnalyse).userAgent("Mozilla/5.9 (Windows NT 6.1; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
@@ -32,23 +36,25 @@ public class WebsiteInformation {
 
             return dataFromWebsite;
 
-        } catch (UnknownHostException e){
-            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            //e.printStackTrace();
 
+        }catch (SocketException e){
+           // e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
-
     }
 
-    /*
-    @ return all the Elements, that are selected
+    /**
+     * pick out the Tag-Information, which we want to
+     *@ return all the Elements, that are selected
      */
     public Elements getSelectedElements(){
         Document allWebsiteInfo = getAllDataFromWebSite();
         if(allWebsiteInfo != null){
-            Elements selectedElements = (Elements)allWebsiteInfo.select(this.selectTagInWebsite);
+            Elements selectedElements = allWebsiteInfo.select(this.selectTagInWebsite);
             return selectedElements;
         } else
             return null;
